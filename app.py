3f86dffcb164b3a64b2993463f9337727e87257e
kamil-iam <greyfox-07@mail.ru>
2022-09-23 17:26:46 +0300

    Interface 2

--- app.py
+++ app.py
@@ -1,15 +1,45 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import *
+import cv2
+
+#root = Tk()
+#from tkFileDialog import ask
 import webbrowser
 
 app = tk.Tk()
 app.title("QR Reader")
+filename = "path"
+#дефайн для path-button
+MyText= StringVar()
+def DisplayDir(Var):
+    feedback = askdirectory()
+    Var.set(feedback)
+def browsefunc():
+    filename = filedialog.askopenfilename()
+    pathlabel.config(text=filename)
 
-search_label = ttk.Label(app, text='Путь')
+search_label = ttk.Label(app, text='Путь к файлу:')
 search_label.grid(row=0, column=0)
+
+
 
 text_field = ttk.Entry(app, width=50)
 text_field.grid(row=0, column=1)
+
+filepath = text_field
+
+
+img = cv2.imread(filename)
+
+detector = cv2.QRCodeDetector()
+# обнаружить и декодировать
+#data, bbox, straight_qrcode = detector.detectAndDecode(img)
+
+data = "Test1"
+dataRes = data
+dataRes_label = ttk.Label(app, text=dataRes)
+dataRes_label.grid(row=1, column=0)
 
 def search():
     if text_field.get().strip() != "":
@@ -23,6 +53,11 @@ def enterBtn(event):
 btn_search = ttk.Button(app, text="Button", width=20, command=searchBtn)
 btn_search.grid(row=1, column=0)
 
+#Отслеживание события нажатия на Enter
 text_field.bind('<Return>', enterBtn)
+
+#Всегда поверх - выключено
+app.wm_attributes('-topmost', False)
+
 
 app.mainloop()
