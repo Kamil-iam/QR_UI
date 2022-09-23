ad40b903a523b97cfe0e549b40c820d2dfb827d9
kamil-iam <greyfox-07@mail.ru>
2022-09-23 12:46:09 +0300

    Interface

--- app.py
+++ app.py
@@ -1,9 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
 import webbrowser
 
 app = tk.Tk()
 app.title("QR Reader")
 
+search_label = ttk.Label(app, text='Путь')
+search_label.grid(row=0, column=0)
 
+text_field = ttk.Entry(app, width=50)
+text_field.grid(row=0, column=1)
+
+def search():
+    if text_field.get().strip() != "":
+        pass
+def searchBtn():
+    search()
+
+def enterBtn(event):
+    pass
+
+btn_search = ttk.Button(app, text="Button", width=20, command=searchBtn)
+btn_search.grid(row=1, column=0)
+
+text_field.bind('<Return>', enterBtn)
 
 app.mainloop()
